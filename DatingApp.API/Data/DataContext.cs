71eacf2930baa24a53e8a8cfda01ccb6710528eb
Martin Oshalim <martinoshalim@gmail.com>
2019-11-05 15:07:05 +1100

    Update Project Added JWT Token Authorization

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -10,12 +10,11 @@ namespace DatingApp.API.Data
     public class DataContext : DbContext
     {
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
         
         public DataContext(DbContextOptions<DataContext> options) : base (options)
         {
         }
-
-
 
     }
 }
